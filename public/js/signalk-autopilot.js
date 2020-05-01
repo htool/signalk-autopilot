1594b97d05a9ca6e96f15826f96182fd3a7c3d25
Hans Tool <hans@hanss-mbp.lan>
2020-05-01 11:08:11 +0200

    Revert because s error signalk-autopilot.js:477 unknown path:steering.autopilot.target.headingMagnetic

--- public/js/signalk-autopilot.js
+++ public/js/signalk-autopilot.js
@@ -101,7 +101,7 @@ var displayByPathParams = {
 const defaultPpreferedDisplayMode = {
   wind: 'environment.wind.angleApparent',
   route: 'navigation.headingMagnetic',
-  auto: 'steering.autopilot.target.headingMagnetic',
+  auto: 'navigation.headingMagnetic',
   standby: 'navigation.headingMagnetic'
 }
 
@@ -383,11 +383,6 @@ var wsConnect = function() {
             },
             {
               "path": "environment.wind.angleTrueWater",
-              "format": "delta",
-              "minPeriod": 900
-            },
-            {
-              "path": "steering.autopilot.target.headingMagnetic",
               "format": "delta",
               "minPeriod": 900
             },
